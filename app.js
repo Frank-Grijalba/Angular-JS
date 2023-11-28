98cb9c492adb8ffb5cef0063c98b62e87156dbfb
frank-grijalba <fjgrijalba@gmail.com>
2023-11-28 09:23:15 -0500

    The first controller

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 (function(){
 
-    
+//'use strict' enabling certain things to just protect us from making mistakes 
+'use strict'
+
+//angular.module('nameApp', dependencies[])    
+angular.module('myFirstApp', [])
+
+.controller('MyFirstController', function(){
+
+})
+
 })();
