10e5f54b85501d84dc1901f114e5b8677d259e33
frank-grijalba <fjgrijalba@gmail.com>
2023-11-30 14:29:25 -0500

    Filter with search

--- Filters/app.js
+++ Filters/app.js
@@ -1,6 +1,15 @@
 (function(){
     'use strict'
 
+    var numberArray = [1, 2,3, 4, 5, 6, 7, 8, 9, 10]
+    
+    var filterNumberArray = numberArray.filter(function(value){
+        return value > 5;
+    });
+
+    console.log ("Filtered number array: ", filterNumberArray )
+    
+    
     angular.module('MsgApp', [])
     .controller('MsgController', MsgController)
     .filter('loves', LovesFilter)
