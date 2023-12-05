b010b7fc04564c142b871c519e15a3f62aa9c772
frank-grijalba <fjgrijalba@gmail.com>
2023-12-05 11:30:15 -0500

    request menu date for category name

--- http-service/app.js
+++ http-service/app.js
@@ -1,39 +1,59 @@
 (function () {
-'use strict';
+  "use strict";
 
-angular.module('MenuCategoriesApp', [])
-.controller('MenuCategoriesController', MenuCategoriesController)
-.service('MenuCategoriesService', MenuCategoriesService)
+  angular
+    .module("MenuCategoriesApp", [])
+    .controller("MenuCategoriesController", MenuCategoriesController)
+    .service("MenuCategoriesService", MenuCategoriesService);
 
+  MenuCategoriesController.$inject = ["MenuCategoriesService"];
+  function MenuCategoriesController(MenuCategoriesService) {
+    var menu = this;
 
-MenuCategoriesController.$inject = ['MenuCategoriesService'];
-function MenuCategoriesController(MenuCategoriesService) {
-  var menu = this;
+    var promise = MenuCategoriesService.getMenuCategories();
 
-  var promise = MenuCategoriesService.getMenuCategories();
+    promise
+      .then(function (response) {
+        menu.categories = response.data.categories;
+      })
+      .catch(function (error) {
+        console.log("Something went terribly wrong.");
+      });
 
-  promise.then(function (response) {
-    menu.categories = response.data.categories;
-  })
-  .catch(function (error) {
-    console.log("Something went terribly wrong.");
-  });
+    menu.logMenuItems = function (categoryName) {
+      var promise = MenuCategoriesService.getMenuForCategory(categoryName);
 
-}
+      promise
+        .then(function (response) {
+          console.log(response.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    };
+  }
 
+  MenuCategoriesService.$inject = ["$http"];
+  function MenuCategoriesService($http) {
+    var service = this;
 
-MenuCategoriesService.$inject = ['$http'];
-function MenuCategoriesService($http) {
-  var service = this;
+    service.getMenuCategories = function () {
+      var response = $http({
+        method: "GET",
+        url: "https://www.themealdb.com/api/json/v1/1/categories.php",
+      });
+      return response;
+    };
 
-  service.getMenuCategories = function () {
-    var response = $http({
-      method: "GET",
-      url: ("https://www.themealdb.com/api/json/v1/1/categories.php")
-    });
-    return response
-  };
-
-}
-
+    service.getMenuForCategory = function (categoryName){
+      var response = $http({
+        method: "GET",
+        url: ("https://www.themealdb.com/api/json/v1/1/filter.php"),
+        params: {
+          c: categoryName
+        }
+      });
+      return response;
+    }
+  }
 })();
